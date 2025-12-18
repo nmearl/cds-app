ae5584695cc6f6f876f5b9a99e17f8ca18cab972
Carifio24 <carifio24@gmail.com>
2025-12-18 13:14:22 +0000

    Remove redundant fit viewer. Add axis padding to base scatter viewer class.

--- packages/cds-hubble/src/cds_hubble/viewers/hubble_fit_viewer.py
+++ packages/cds-hubble/src/cds_hubble/viewers/hubble_fit_viewer.py
@@ -1,32 +1,11 @@
-from echo import delay_callback
 from glue_plotly.viewers.scatter.viewer import PlotlyScatterView
 from .hubble_scatter_viewer import HubbleScatterViewerState
 from cds_core.viewers import cds_viewer
 
 __all__ = [
-    "HubbleFitView",
     "HubbleFitLayerView",
 ]
 
-
-class HubbleFitViewerState(HubbleScatterViewerState):
-
-    def reset_limits(self, visible_only=None):
-        with delay_callback(self, "x_min", "x_max", "y_min", "y_max"):
-            super().reset_limits(visible_only=visible_only)
-            if self.x_max is not None:
-                self.x_max = 1.1 * self.x_max
-            if self.y_max is not None:
-                self.y_max = 1.1 * self.y_max
-
-
-HubbleFitView = cds_viewer(
-    PlotlyScatterView,
-    name="HubbleFitView",
-    viewer_tools=["plotly:home", "plotly:zoom", "hubble:linefit"],
-    label="Fit View",
-    state_cls=HubbleFitViewerState,
-)
 
 HubbleFitLayerView = cds_viewer(
     PlotlyScatterView,
@@ -35,5 +14,5 @@ HubbleFitLayerView = cds_viewer(
         "hubble:linefit",
     ],
     label="Layer View",
-    state_cls=HubbleFitViewerState,
+    state_cls=HubbleScatterViewerState,
 )
