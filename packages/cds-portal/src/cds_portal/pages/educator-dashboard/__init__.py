7ed231c2089ee95aa73731879e5c5adbfece17ed
John Arban Lewis <johnarban@users.noreply.github.com>
2026-01-21 12:57:44 -0500

    show class name and info in dashboard

--- packages/cds-portal/src/cds_portal/pages/educator-dashboard/__init__.py
+++ packages/cds-portal/src/cds_portal/pages/educator-dashboard/__init__.py
@@ -10,13 +10,15 @@ def Page():
 
     classes_dict = BASE_API.load_educator_classes()
     educator_class_ids = [str(cls["id"]) for cls in classes_dict["classes"]]
+    educator_class_info = [cls for cls in classes_dict["classes"]]
+
 
     with solara.Row(classes=["fill-height"]):
         with rv.Col(cols=12):
-            solara.Div("Educator Dashboard", classes=["display-1", "mb-8"])
+            solara.Div("Educator Dashboard", classes=["display-1"])
 
             if url_params.get("id") not in educator_class_ids:
                 solara.Markdown("You do not have access to this class.")
                 return
             else:
-                EducatorDashboard(url_params)
+                EducatorDashboard(url_params, class_info_list=educator_class_info)
