dcd32d371403bf9ea044830c46df9f8b9e841d2c
Nicholas Earl <contact@nicholasearl.me>
2026-02-05 14:17:54 -0600

    Remove memory auto scaling policies from Terraform configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -960,41 +960,6 @@ resource "aws_appautoscaling_policy" "cds_hubble_cpu" {
   }
 }
 
-# Auto Scaling Policies - Memory Based
-resource "aws_appautoscaling_policy" "cds_portal_memory" {
-  name               = "${var.environment}-cds-portal-memory-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.cds_portal.resource_id
-  scalable_dimension = aws_appautoscaling_target.cds_portal.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.cds_portal.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = 80.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
-  }
-}
-
-resource "aws_appautoscaling_policy" "cds_hubble_memory" {
-  name               = "${var.environment}-cds-hubble-memory-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.cds_hubble.resource_id
-  scalable_dimension = aws_appautoscaling_target.cds_hubble.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.cds_hubble.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = 80.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
-  }
-}
-
 # Generate a random secret for CloudFront
 resource "random_password" "cloudfront_secret" {
   count   = var.cloudfront_secret == "" ? 1 : 0
