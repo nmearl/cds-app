2c018c175d9abbe3c9241bb44d29f24371e471a1
Nicholas Earl <contact@nicholasearl.me>
2026-02-24 11:08:01 -0600

    Add demo environment variable support for routing configuration

--- packages/cds-hubble/src/cds_hubble/server.py
+++ packages/cds-hubble/src/cds_hubble/server.py
@@ -3,17 +3,25 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Mount, Route
 from solara.server import settings
+import os
 
 import solara.server.starlette
+
+FORCE_DEMO = os.getenv("CDS_FORCE_DEMO", "false").strip().lower() == "true"
 
 
 def root(request: Request):
     return JSONResponse({"Error Message": "Go back whence ye came."})
 
 
-routes = [
-    Route("/", endpoint=root),
-    Mount("/hubbles-law/", routes=solara.server.starlette.routes),
-]
+if FORCE_DEMO:
+    routes = [
+        Mount("/", routes=solara.server.starlette.routes),
+    ]
+else:
+    routes = [
+        Route("/", endpoint=root),
+        Mount("/hubbles-law/", routes=solara.server.starlette.routes),
+    ]
 
 app = Starlette(routes=routes, middleware=solara.server.starlette.middleware)
